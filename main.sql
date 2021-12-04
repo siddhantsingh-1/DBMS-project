3129c3b4194c2e300d42fb212eca5305409efdcc
Arjun Gaur <81460828+Proarj@users.noreply.github.com>
2021-12-04 10:47:58 +0530

    Update main.sql

--- main.sql
+++ main.sql
@@ -1,13 +1,21 @@
-CREATE TABLE user(
+CREATE TABLE user_info(
     username varchar(20) PRIMARY KEY,
     name varchar(30)
     );
+
 CREATE TABLE account(
     account_id varchar(13) PRIMARY KEY,
     cash_balance bigint,
     investment_value bigint,
-    total_value int GENERATED ALWAYS AS (cash_balance + investment_value) STORED
-     );
+    total_value bigint GENERATED ALWAYS AS (cash_balance + investment_value) STORED
+    );
+
+
+CREATE TABLE instrument(
+    symbol varchar(10) PRIMARY KEY,
+    quote int
+);
+
 
 CREATE TABLE trade(
     ID int PRIMARY KEY,
@@ -15,7 +23,7 @@ CREATE TABLE trade(
     account_id varchar(13),
     shares int,
     price int,
-    amount bigint GENERATED ALWAYS AS(shares*prices) STORED,
+    amount bigint GENERATED ALWAYS AS(shares*price) STORED,
     date date,
     FOREIGN KEY(account_id) REFERENCES account(account_id) ON DELETE SET NULL,
     FOREIGN KEY(symbol) REFERENCES instrument(symbol) ON DELETE SET NULL
@@ -29,26 +37,23 @@ CREATE TABLE sell(
     ID int PRIMARY KEY
 );
 
-CREATE TABLE instrument(
-    symbol varchar(10) PRIMARY KEY,
-    quote int
-)
+
 
 CREATE TABLE owns(
     account_id varchar(13) REFERENCES account(account_id),
     instrument_symbol varchar(10) REFERENCES instrument(symbol),
-    current_value bigint AS (quantity*instrument(quote)),
+    current_value bigint,
     quantity int 
 );
 
 CREATE TABLE stock(
-    symbol varchar(10) PRIMARY KEY,
+    symbol varchar(10) PRIMARY KEY
 );
 
 CREATE TABLE mutual_fund(
-    symbol varchar(10) PRIMARY KEY,
+    symbol varchar(10) PRIMARY KEY
 );
 
 CREATE TABLE etf(
-    symbol varchar(10) PRIMARY KEY,
+    symbol varchar(10) PRIMARY KEY
 );
