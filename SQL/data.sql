204cb628f2c9932cb6f8415d343c00ea61326efe
Siddhant Singh <93873061+siddhantsingh-1@users.noreply.github.com>
2021-12-08 15:31:12 +0530

    Update data.sql

--- SQL/data.sql
+++ SQL/data.sql
@@ -2,7 +2,7 @@ INSERT INTO user VALUES ('siddhantsingh-1', 'Siddhant Singh');
 INSERT INTO account(account_id,username,cash_balance,investment_value) VALUES (1, 'siddhantsingh-1', 0, 150000);
 INSERT INTO account(account_id,username,cash_balance,investment_value) VALUES (3, 'siddhantsingh-1', 140000, 60000);
 
-INSERT INTO user VALUES ('proarj', "Arjun Gaur");
+INSERT INTO user VALUES ('proarj', 'Arjun Gaur');
 INSERT INTO account(account_id,username,cash_balance,investment_value) VALUES(2, 'proarj', 40000, 75000);
 
 INSERT INTO user VALUES('niharika-2206', 'Niharika Upadhayay');
@@ -56,3 +56,18 @@ INSERT INTO etf VALUES('KOTAKPSUBK',13, 'Kotak AMC', 'NIFTY PSU BANK', '2007-11-
 
 INSERT INTO instrument(instrument_id, quote) VALUES(14, 31.78);
 INSERT INTO etf VALUES('MOM100',14, 'Motilal Oswal AMC', '	NIFTY Midcap 100', '2011-01-31');
+
+--Inserting Trades
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(1,1,600,200,'2021-01-02',1,5);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(2,1,500,250,'2020-10-11',2,4);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(3,2,10,1000,'2021-01-11',1,5);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(4,1,400,800,'2021-08-18',1,11);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(5,1,400,800,'2021-08-18',3,11);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(6,2,40,700,'2021-09-18',3,11);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(7,1,100,750,'2021-07-18',2,13);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(8,1,150,650,'2020-09-16',4,7);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(9,1,200,550,'2021-05-11',4,9);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(10,1,200,400,'2021-09-13',3,5);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(11,1,200,250,'2021-09-14',2,5);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(12,1,200,200,'2021-09-13',4,5);
+INSERT INTO TRADE(trade_id,buy_or_sell,shares,price,trade_date,account_id,instrument_id) VALUES(13,2,40,300,'2021-07-18',4,7);
